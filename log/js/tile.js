a28e104e5cce86e4f5b6af0b1ebf28d98db0ac0b
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-11-25 21:36:28 -0800

    Add 2048-log to the log/ directory.

---
+++ log/js/tile.js
@@ -0,0 +1,35 @@
+
+function Tile(position, type, value) {
+  this.x                = position ? position.x : null;
+  this.y                = position ? position.y : null;
+  this.type             = type;
+  this.value            = value;
+  this.is_heavy         = false;  // by default
+
+  this.score            = value;  // by default
+
+  this.accumulatedScore = 0;    // Tracks score to lose when square-rooting this tile
+  this.previousPosition = null;
+  this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
+
+Tile.prototype.serialize = function () {
+  return {
+    position: {
+      x: this.x,
+      y: this.y
+    },
+    type: this.type,
+    value: this.value,
+    is_heavy: this.is_heavy
+  };
+};
